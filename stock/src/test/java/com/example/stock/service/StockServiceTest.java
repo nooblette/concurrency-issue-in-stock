9925a2482ee213337f951e340872c2e2dd81ded6
nooblette <min9hyuk@gmail.com>
2024-08-07 07:12:50 +0900

    feat : 문제점

--- stock/src/test/java/com/example/stock/service/StockServiceTest.java
+++ stock/src/test/java/com/example/stock/service/StockServiceTest.java
@@ -2,6 +2,10 @@ package com.example.stock.service;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -39,5 +43,35 @@ class StockServiceTest {
 		// then : 1L 상품의 재고 : 100 - 1 = 99개가 남아있어야함
 		Stock stock = stockRepository.findById(1L).orElseThrow();
 		assertEquals(99, stock.getQuantity());
+	}
+
+	@Test
+	public void 동시에_100개의_요청() throws InterruptedException {
+		// when
+		// 100개의 쓰레드 사용(멀티스레드)
+		int threadCount = 100;
+
+		// ExecutorService : 비동기로 실행하는 작업을 간단하게 실행할 수 있도록 자바에서 제공하는 API
+		ExecutorService executorService = Executors.newFixedThreadPool(32);
+
+		// CountDownLatch : 작업을 진행중인 다른 스레드가 작업을 완료할때까지 대기할 수 있도록 도와주는 클래스
+		CountDownLatch latch = new CountDownLatch(threadCount);
+		// 100개의 작업 요청
+		for(int i = 0; i < threadCount; i++) {
+			executorService.submit(() -> {
+				try {
+					stockService.decrease(1L, 1L);
+				} finally {
+					latch.countDown();
+				}
+			});
+		}
+		latch.await();
+
+		// then
+		Stock stock = stockRepository.findById(1L).orElseThrow();
+		// 처음에 100개로 저장하고 한개씩 100번 감속하기때문에 0이 될 것을 예상
+		assertEquals(0, stock.getQuantity());
+
 	}
 }
